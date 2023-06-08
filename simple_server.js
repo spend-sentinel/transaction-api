4cf342001a8bedefbbf6264bdf4d767c390c4e10
Maor Ben Noach <bennoachmaor@gmail.com>
2023-06-08 16:54:54 +0300

    Seperated module, exception handling, status codes
    - Added catches for write and read from files.
    - Moved all handlers to connectionHandlers.js
    - Added status codes for different errors.

--- simple_server.js
+++ simple_server.js
@@ -1,77 +1,10 @@
-const crud = require('./crud.js'); // CreateNew, Get(Specific/All), DeleteSpecific, UpdateSpecific
 const express = require('express'); // express()
-const bodyParser = require('body-parser'); // bodyParser.json
-
 const serverApplication = express(); // use, post, delete, get, delete, listen
-const jsonParser = bodyParser.json();
-
-// transactions/1
-
-//  Functions
-const ExtractTransactionNumber = route => {
-    return parseInt(route.slice(1));
-}
-
-const GetRoute = req => {
-    return (req.params['0']);
-}
-
-
+const connectHandler = require('./connectionHandlers.js'); // CreateNew, Get(Specific/All), DeleteSpecific, UpdateSpecific
 // Server Configuration
 const portNum = 8080;
-serverApplication.use(bodyParser.json());
-
-serverApplication.post('/', (req, res) => { // Handles POST request for inserting a new transaction
-    const status = crud.CreateNewEntry(req.body)
-    if (!status) {
-        res.end("New transaction submitted")
-    } else if (1 == status) { 
-        res.end("Transaction was already found in current data");
-    } else {
-        res.end("An error has occured while submitting the new transaction")
-    }
-})
-
-serverApplication.put('/', (req, res) => { // Handles PUT request for updating an existing transaction
-    const status = crud.UpdateTransactionserverApplicationroval(req.body['TransNum'], req.body['Status']);
-    if (!status) {
-        res.end("serverApplicationroval of transaction updated")
-    } else if (-1 == status) {
-        res.end("An error has occured while updating the tranaction")
-    } else {
-        res.end("Transaction not found in attempt to update");
-    }
-})
-
-serverApplication.get("*", (req, res)=>{ // Handles GET transactions
-    const route = GetRoute(req);
-    if (1 == route.length){ // No text after '/'
-        res.end(JSON.stringify(crud.GetAllTransactions(), null, 2));
-    } else {
-        const transaction = crud.GetSpecificTransaction(ExtractTransactionNumber(route));
-        if (null != transaction) {
-            res.end(JSON.stringify(transaction, null, 2));
-        } else {
-            res.end("Transaction not found");
-        }
-    }
-})
-
-serverApplication.delete("*", (req, res)=>{ // Handles DELETE with specific transacton
-    const route = GetRoute(req);
-    if (1 == route.length){ // No text after '/'
-        res.end("Transaction number unspecified");
-    } else {
-        const transactionNumber = ExtractTransactionNumber(route);
-        if (crud.DeleteTransaction(transactionNumber)) {
-            res.end("Transaction #" + transactionNumber + " not found");
-        } else {
-            res.end("Removed transaction #" + transactionNumber);
-        }
-    }
-})
+connectHandler.ConfigureHandlers(serverApplication);
 // End of server configuration
-
-serverApplication.listen(portNum, () => {
+serverApplication.listen(portNum, () => { // Start activity of server
     console.log(`Server is running at http://localhost:${portNum}`);
 });
