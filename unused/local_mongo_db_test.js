04ebc4ec96c196a194bbaae00b0600b5b5b023f7
Bennoama <120720815+Bennoama@users.noreply.github.com>
2023-07-16 13:35:37 +0300

    Delete unused directory

--- unused/local_mongo_db_test.js
+++
@@ -1,8 +0,0 @@
-const MongoClient = require('mongodb').MongoClient;
-
-MongoClient.connect("mongodb://localhost:27017").then((client) => {
-    console.log("Connected");
-})
-.catch(err => {
-    console.error(err);
-});
