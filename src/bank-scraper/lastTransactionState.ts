e5ba25a2b9605a51d716fbe02fa49536dcd06191
Maor Ben Noach <bennoachmaor@gmail.com>
2024-03-30 14:23:34 +0300

    Create lastTransactionState.ts

---
+++ src/bank-scraper/lastTransactionState.ts
@@ -0,0 +1,17 @@
+import { filePath } from "./scrape-details";
+import fs from 'fs'
+
+export const getLastTransactionDate = ():Date => {
+    if (!fs.existsSync(filePath)) {
+      fs.writeFileSync(filePath, "0");
+    }
+    const data = fs.readFileSync(filePath);
+    if (!data) {
+      return new Date(0);
+    }
+    return new Date(+data);
+  }
+  
+export const updateLatestTransactionDate = (latestTransactionDate: Date) => {
+    fs.writeFileSync(filePath, (1000 + latestTransactionDate.getTime()).toString())  
+  }
