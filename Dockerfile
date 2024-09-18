97e64dd2dd96aceb74023516af828ed007e46cde
Bennoama <120720815+Bennoama@users.noreply.github.com>
2024-09-18 22:16:18 +0300

    Fixed to PR review
    1. Wait for server closing before disconnecting the db
    2. healthcheck 1-liner
    3. Logging moved to properties

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,17 @@
-FROM node:20-alpine as base
+FROM node:20-alpine AS base
 WORKDIR /service
 
-FROM base as dependencies
+FROM base AS dependencies
 COPY package.json yarn.lock tsconfig.json ./
 RUN yarn --pure-lockfile --production true
 
-FROM dependencies as build
+FROM dependencies AS build
 RUN yarn --pure-lockfile --production false
 COPY src ./src
 
 RUN yarn build
 
-FROM base as release
+FROM base AS release
 COPY --from=dependencies /service/node_modules ./node_modules
 COPY --from=dependencies /service/package.json ./package.json
 COPY --from=build /service/dist ./dist
--- src/framework/gracefulShutdown.ts
+++ src/framework/gracefulShutdown.ts
@@ -4,32 +4,22 @@ import * as mongo from "./mongo.js";
 import { createTerminus, TerminusOptions } from "@godaddy/terminus";
 import { isDBHealthy } from "./crud-db.js";
 
-const loggerWrapper = (msg: string, err: Error) => {
-  if (err) {
-    logger.error(err.message);
-  } else {
-    logger.info(msg);
-  }
-};
-
 export const gracefulShutdown = (app: Application) => {
   const terminusOptions: TerminusOptions = {
     onShutdown: async () => {
       logger.info("Server shutting down");
     },
     onSignal: async () => {
-      await Promise.all([app.close(), mongo.disconnectDB()]);
+      logger.info(
+        "Received closing signal. shutting down server and disconnecting the db.",
+      );
+      await app.close();
+      await mongo.disconnectDB();
     },
     healthChecks: {
-      "/healthcheck": async () => {
-        const response = {
-          dbHealthy: await isDBHealthy(),
-        };
-        return response;
-      },
+      "/healthcheck": async () => ({ dbHealthy: await isDBHealthy() }),
     },
     caseInsensitive: true,
-    logger: loggerWrapper,
   };
   createTerminus(app.server, terminusOptions);
 };
