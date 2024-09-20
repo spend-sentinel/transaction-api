817bc2b73750d380ab8cc49cf337b4c84b3cf9dd
Bennoama <120720815+Bennoama@users.noreply.github.com>
2024-09-20 18:13:08 +0300

    Refactored route setting

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
-import { fastify } from "fastify";
 import { setUpServer } from "./server/index.js";
 import * as mongo from "./framework/mongo.js";
 import { logger } from "./framework/logger.js";
 
-const application = fastify({
-  logger,
-});
-
 const main = async () => {
   logger.info("Connectiong to db...");
   await mongo.connectToDB();
-  setUpServer(application);
+  setUpServer();
 };
 
 main().catch(console.error);
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,19 +1,19 @@
-import { setRoutes } from "./routes.js";
+import { application, setRoutes } from "./routes.js";
 import { gracefulShutdown } from "../framework/gracefulShutdown.js";
 import { port, address } from "../framework/environment.js";
 import { logger } from "../framework/logger.js";
-import { Application, ServerParams } from "../shared/types.js";
+import { ServerParams } from "../shared/types.js";
 
 const serverParams: ServerParams = {
   host: address,
   port: port,
 };
 
-export const setUpServer = (application: Application) => {
-  setRoutes(application);
+export const setUpServer = () => {
+  setRoutes();
   application.listen(serverParams, () => {
     // Start activity of server
     logger.info({ address, port }, `server has started running`);
   });
-  gracefulShutdown(application);
+  gracefulShutdown();
 };
